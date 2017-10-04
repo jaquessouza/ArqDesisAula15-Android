f473e641d5d996afe1d213c8f8aabcd34777fe9a
Jaques Silva <jaquesquinho@gmail.com>
2017-10-04 20:21:21 -0300

    Initial commit

---
+++ app/src/main/java/com/usjt/caixaeletronico/Login.java
@@ -0,0 +1,24 @@
+package com.usjt.caixaeletronico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.if );
+
+    }
+
+    //acess to account
+    public void acessAccount(View view){
+
+        Intent intent = new Intent(this, Account.class);
+
+        startActivity(intent);
+    }
+}
